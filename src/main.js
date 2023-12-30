6047e4cbb7aeb9fe5541fbe4bb480d99740550ef
mullerdaniil <mullerdaniil@mail.ru>
2023-12-30 14:03:33 +0300

    fix: edit backend url (test)

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App);
-app.config.globalProperties.BACKEND_URL = 'https://chatus-backend.onrender.com';
+// app.config.globalProperties.BACKEND_URL = 'https://chatus-backend.onrender.com';
+app.config.globalProperties.BACKEND_URL = 'http://localhost:8080';
 app.mount('#app');
